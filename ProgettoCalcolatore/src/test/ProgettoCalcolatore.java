037eb7bf68b13b9b66d3ebc6d8b84c873061276c
SchiavoniDaniele <schiavonidaniele@gmail.com>
2024-06-27 14:48:34 +0200

    Aggiunti casi
    Aggiunti casi 1,2,3,4

--- ProgettoCalcolatore/src/test/ProgettoCalcolatore.java
+++ ProgettoCalcolatore/src/test/ProgettoCalcolatore.java
@@ -27,16 +27,113 @@ public class ProgettoCalcolatore {
 
 				switch (scelta) {
 				case 1:
-
+					boolean condizioneLatoQ=true;//condizione per uscire dalla sezione
+					do {
+						System.out.print("Inserisci il valore del lato: ");
+						if(!input.hasNextDouble()) {//check double
+							System.out.println("Inserisci un dato valido!");
+							input.next();
+						}else {
+							double latoQuadrato=input.nextDouble();
+							if (latoQuadrato>0) {//se il lato è positivo si può proseguire
+							System.out.println("Il perimetro del quadrato è uguale a: "+latoQuadrato*4+
+									"\nL'area del quadrato è uguale a:: "+latoQuadrato*latoQuadrato);
+							condizioneLatoQ=false;
+							}
+							else {
+								System.out.println("Inserisci un dato valido!");
+							}
+						}
+					}while(condizioneLatoQ);
 					break;
 				case 2:
-
+					boolean condizioneBaseR=true;
+					boolean condizioneAltezzaR=true;
+					do {
+						System.out.print("Inserisci il valore della base: ");
+						if(!input.hasNextDouble()) {
+							System.out.println("Inserisci una scelta valida");
+							input.next();
+						}else {
+							double baseR=input.nextDouble();
+							if(baseR>0) {
+								do {
+									System.out.print("Inserisci il valore dell'altezza: ");
+									if(!input.hasNextDouble()) {
+										System.out.println("Inserisci una scelta valida");
+										input.next();
+									}else {
+										double altezzaR=input.nextDouble();
+										if(altezzaR>0) {
+										System.out.println("Il perimetro del rettangolo è uguale a: "+(altezzaR+baseR)*2+
+												"\nL'area del rettangolo è uguale a:: "+baseR*altezzaR);
+										condizioneAltezzaR=false;
+										}else {
+											System.out.println("Inserisci una scelta valida");
+										}
+									}
+								}while(condizioneAltezzaR);
+								condizioneBaseR=false;
+	
+								}else {
+									System.out.println("Inserisci una scelta valida");
+								}
+						}
+					}while(condizioneBaseR);
 					break;
 				case 3:
+					boolean condizioneRaggioC=true;
+					do {
+						System.out.print("Inserire il raggio del cerchio: ");
+						if(!input.hasNextDouble()) {
+							System.out.println("Inserire un dato valido!");
+							input.next();
 
+						}else {
+							double raggioCerchio=input.nextDouble();
+							if(raggioCerchio>0) {
+							System.out.println("Il circonferenza del cerchio è uguale a: "+(raggioCerchio*2*3.14)+
+									"\nL'area del cerchio è uguale a:: "+(raggioCerchio*raggioCerchio*3.14));
+							condizioneRaggioC=false;
+							}else {
+								System.out.println("Inserisci una scelta valida");
+							}
+						}
+					}while(condizioneRaggioC);
 					break;
 				case 4:
-
+					boolean condizioneLatoT=true;
+					boolean condizioneAltezzaT=true;
+					do {
+						System.out.print("Inserisci il valore del lato: ");
+						if(!input.hasNextDouble()) {
+							System.out.println("Inserisci una scelta valida");
+							input.next();
+						}else {
+							double latoTriangolo=input.nextDouble();
+							if(latoTriangolo>0) {
+							do {
+								System.out.print("Inserisci il valore dell'altezza: ");
+								if(!input.hasNextDouble()) {
+									System.out.println("Inserisci una scelta valida");
+									input.next();
+								}else {
+									double altezzaTriangolo=input.nextDouble();
+									if (altezzaTriangolo>0) {
+									System.out.println("Il perimetro del triangolo equilatero è uguale a: "+latoTriangolo*3+
+											"\nL'area del rettangolo è uguale a:: "+latoTriangolo*altezzaTriangolo/2);
+									condizioneAltezzaT=false;
+									}else {
+										System.out.println("Inserisci una scelta valida");
+									}
+								}
+							}while(condizioneAltezzaT);
+							condizioneLatoT=false;
+							}else {
+								System.out.println("Inserisci una scelta valida");
+							}
+						}
+					}while (condizioneLatoT);
 					break;
 				case 5:
 
